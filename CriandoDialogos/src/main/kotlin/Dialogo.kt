191e20aa85ddf61e13e1476e0b338ca131452f64
Joolmaa <102316347+Joolmaa@users.noreply.github.com>
2022-11-15 00:20:38 -0300

    Add files via upload

---
+++ CriandoDialogos/src/main/kotlin/Dialogo.kt
@@ -0,0 +1,44 @@
+fun main() {
+
+    println("oioioi....")
+    println("Qual o seu nome? ")
+
+    var nome: String = readLine()!!.toString()
+
+    println("O meu é Jef, $nome. Muito prazer")
+
+    println("Qual sua idade $nome?")
+
+    var idade: Int = readLine()!!.toInt()
+
+    println("Eu n sei a minha hihi")
+
+    println("Oq vc faz da vida $nome??")
+
+    var vida: String = readLine()!!.toString()
+
+    println("nossa que merda hém kkkkkkkkk")
+
+    println("Qual é a sua melhor amiga?")
+
+    var amiga: String = readLine()!!.toString()
+
+    println("Ela é muito legal msm, ouvi falar que ela é vidente kkkk")
+
+    println("Quais são as suas 3 melhores amigas????")
+
+    var segunda: String = readLine()!!.toString()
+
+    println("Me fale o nome delas $nome")
+
+    var Nome: String = readLine()!!.toString()
+
+    println("Ouvi falar que essa Roberta é bem brava kkkkkkk")
+
+    println("EU vou indo . . .\nTenho mais oq fazer da vida XD")
+
+}
+
+
+
+
